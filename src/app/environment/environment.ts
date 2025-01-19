9a0b9d5ee03d68e830cd1e4ca06c774fcb510231
BekaBaydullaev <bekabaydullaev@gmail.com>
2025-01-20 04:42:04 +0500

    minor adjustments

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://rest-items.research.cloudonix.io/items'
+  };
+  
--- src/app/service/product-list.service.ts
+++ src/app/service/product-list.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../interfaces/product.interface';
-import { environment } from '../environments/environments';
+import { environment } from '../environment/environment';
 
 
 @Injectable({ providedIn: 'root' })
